9a43e49ae6849b990a6d4d316857977d50591839
Feng, Bin <bfeng@nltechdev.com>
2025-01-23 14:32:13 +0800

    clear repo

--- quotation-api/sql/2025-01-22.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE quote_email_setting ADD category text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '分类';
--- quotation-web/src/api/lose.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'; 
-// 丢单
-export const fetchLoseInfo = (path,params) => {  
-    return request({  
-      url: `/quote/lose/${path}`,  
-      method: 'post',  
-      data: params
-    });  
-  };
